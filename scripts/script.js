bffe938ced5fc0a9a1d9784c42b48d91720a555e
Mohammedjaseem <56159302+Mohammedjaseem@users.noreply.github.com>
2021-03-22 15:15:42 +0530

    updated
    aswin proftfolio

--- scripts/script.js
+++ scripts/script.js
@@ -1,231 +1,231 @@
 window.onload = function() {
-	let requestAnimationFrame,
-		requestId,
-		canvasMain,
-		ctxMain,
-		width,
-		height,
-		dots = [],
-		n = 22,
-		dist = 260,
-		snake,
-		tilesVertical = 0,
-		tilesHorizontal = 0,
-		updatedTime,
-		pageIndicators = document.querySelectorAll('.indicator'),
-		contactTypes = document.querySelectorAll('.contact-icon');
+    let requestAnimationFrame,
+        requestId,
+        canvasMain,
+        ctxMain,
+        width,
+        height,
+        dots = [],
+        n = 22,
+        dist = 260,
+        snake,
+        tilesVertical = 0,
+        tilesHorizontal = 0,
+        updatedTime,
+        pageIndicators = document.querySelectorAll('.indicator'),
+        contactTypes = document.querySelectorAll('.contact-icon');
 
-	let canvasType = CANVAS_ASTEROIDS;
+    let canvasType = CANVAS_ASTEROIDS;
 
-	let spaceShip, asteroids = [];
-	let asteroidKeyControl = -1
-	let asteroidCount = 10
+    let spaceShip, asteroids = [];
+    let asteroidKeyControl = -1
+    let asteroidCount = 10
 
-	pageIndicators[0].addEventListener('click', (evt => {
-		setCanvasType(CANVAS_PARTICLES)
-		setIndicator(canvasType)
-	}));
+    pageIndicators[0].addEventListener('click', (evt => {
+        setCanvasType(CANVAS_PARTICLES)
+        setIndicator(canvasType)
+    }));
 
-	pageIndicators[1].addEventListener('click', (evt => {
-		setCanvasType(CANVAS_SNAKE)
-		setIndicator(canvasType)
-	}));
+    pageIndicators[1].addEventListener('click', (evt => {
+        setCanvasType(CANVAS_SNAKE)
+        setIndicator(canvasType)
+    }));
 
-	pageIndicators[2].addEventListener('click', (evt => {
-		setCanvasType(CANVAS_ASTEROIDS)
-		setIndicator(canvasType)
-	}));
+    pageIndicators[2].addEventListener('click', (evt => {
+        setCanvasType(CANVAS_ASTEROIDS)
+        setIndicator(canvasType)
+    }));
 
-	contactTypes[0].addEventListener('click', (e => {
-		openHyperLink("https://github.com/Jaseemakhtar")
-	}))
+    contactTypes[0].addEventListener('click', (e => {
+        openHyperLink("https://github.com/azwisec")
+    }))
 
-	contactTypes[1].addEventListener('click', (e => {
-		openHyperLink("https://t.me/Jaseemakhtar")
-	}))
+    contactTypes[1].addEventListener('click', (e => {
+        openHyperLink("https://www.linkedin.com/in/aswin-govind/")
+    }))
 
-	contactTypes[2].addEventListener('click', (e => {
-		openHyperLink("https://mail.google.com/mail/?view=cm&fs=1&to=jaseemamaljeri@gmail.com")
-	}))
+    contactTypes[2].addEventListener('click', (e => {
+        openHyperLink("https://mail.google.com/mail/?view=cm&fs=1&to=aswingovind137@gmail.com")
+    }))
 
-	function openHyperLink(link) {
-		window.open(link, '_blank');
-	}
+    function openHyperLink(link) {
+        window.open(link, '_blank');
+    }
 
-	function setCanvasType(pos) {
-		cancelAnimationFrame(requestId)
-		canvasType = pos
-		draw()
-	}
+    function setCanvasType(pos) {
+        cancelAnimationFrame(requestId)
+        canvasType = pos
+        draw()
+    }
 
-	function setIndicator(index) {
-		for (let i = 0; i < pageIndicators.length; i++) {
-			if (i == index) {
-				pageIndicators[index].children[IMAGE_INDEX].classList.remove('hidden')
-				pageIndicators[index].children[DIV_INDEX].classList.add('glow')
-			} else {
-				pageIndicators[i].children[IMAGE_INDEX].classList.add('hidden')
-				pageIndicators[i].children[DIV_INDEX].classList.remove('glow')
-			}
-		}
-	}
+    function setIndicator(index) {
+        for (let i = 0; i < pageIndicators.length; i++) {
+            if (i == index) {
+                pageIndicators[index].children[IMAGE_INDEX].classList.remove('hidden')
+                pageIndicators[index].children[DIV_INDEX].classList.add('glow')
+            } else {
+                pageIndicators[i].children[IMAGE_INDEX].classList.add('hidden')
+                pageIndicators[i].children[DIV_INDEX].classList.remove('glow')
+            }
+        }
+    }
 
-	function setupEnvironment() {
-		requestAnimationFrame = window.requestAnimationFrame || window.mozRequestAnimationFrame 
-		|| window.webkitRequestAnimationFrame || window.msRequestAnimationFrame
-		
-		canvasMain = document.getElementById("canvasMain")
+    function setupEnvironment() {
+        requestAnimationFrame = window.requestAnimationFrame || window.mozRequestAnimationFrame ||
+            window.webkitRequestAnimationFrame || window.msRequestAnimationFrame
 
-		ctxMain = canvasMain.getContext("2d")
-	}
+        canvasMain = document.getElementById("canvasMain")
 
-	function setupCanvas() {
-		width = window.innerWidth
-		height = window.innerHeight
-		
-		canvasMain.width = width
-		canvasMain.height = height
+        ctxMain = canvasMain.getContext("2d")
+    }
 
-		let widthRem = width % TILE_SIZE
-		let heightRem = height % TILE_SIZE
-		let gameWidth = width
-		let gameHeight = height
+    function setupCanvas() {
+        width = window.innerWidth
+        height = window.innerHeight
 
-		spaceShip = new SpaceShip(ctxMain, 16, width, height)
-		for (let i = 0; i < asteroidCount; i++) {
-			asteroids.push(new Asteroid(ctxMain, 28, width, height))
-		}
+        canvasMain.width = width
+        canvasMain.height = height
 
-		if (widthRem != 0) {
-			gameWidth = width - widthRem
-		}
+        let widthRem = width % TILE_SIZE
+        let heightRem = height % TILE_SIZE
+        let gameWidth = width
+        let gameHeight = height
 
-		if (heightRem != 0) {
-			gameHeight = height - heightRem
-		}
+        spaceShip = new SpaceShip(ctxMain, 16, width, height)
+        for (let i = 0; i < asteroidCount; i++) {
+            asteroids.push(new Asteroid(ctxMain, 28, width, height))
+        }
 
-		tilesHorizontal = gameWidth / TILE_SIZE
-		tilesVertical = gameHeight / TILE_SIZE
-		
-		snake = new Snake(ctxMain, tilesHorizontal, tilesVertical, [widthRem / 2, heightRem / 2])
-		updatedTime = Date.now()
+        if (widthRem != 0) {
+            gameWidth = width - widthRem
+        }
 
-		for (let i = 0; i < n; i++) {
-			let offset = 14
-			let x = getRandomIntFromInterval(offset, width - offset)
-			let y = getRandomIntFromInterval(offset, height - offset)
-			let dot = new Dot(x, y, ctxMain)
-			dots.push(dot)
-		}
-	}
+        if (heightRem != 0) {
+            gameHeight = height - heightRem
+        }
 
-	function draw(){
-		if (canvasType == CANVAS_PARTICLES) {
-			drawParticles()
-		} else if (canvasType == CANVAS_SNAKE){
-			drawSnake()
-		} else {
-			drawAsteroids()
-		}
-	}
+        tilesHorizontal = gameWidth / TILE_SIZE
+        tilesVertical = gameHeight / TILE_SIZE
 
-	function drawParticles() {
-		ctxMain.clearRect(0,0, width, height)
-		for(let i=0; i<dots.length; i++){
-			for(let j=0; j<dots.length; j++){
-				if (i != j) {
-					dots[j].check(dots[i], dist)
-				}
-			}
-			dots[i].show()
-			dots[i].update()
-		}
-		requestId = requestAnimationFrame(drawParticles)
-	}
+        snake = new Snake(ctxMain, tilesHorizontal, tilesVertical, [widthRem / 2, heightRem / 2])
+        updatedTime = Date.now()
 
-	function drawSnake() {
-		let currentTime = Date.now()
+        for (let i = 0; i < n; i++) {
+            let offset = 14
+            let x = getRandomIntFromInterval(offset, width - offset)
+            let y = getRandomIntFromInterval(offset, height - offset)
+            let dot = new Dot(x, y, ctxMain)
+            dots.push(dot)
+        }
+    }
 
-		if ((currentTime - updatedTime) >= 200) {
-			ctxMain.clearRect(0, 0, width, height)
-			
-			snake.update()
-			snake.show()
+    function draw() {
+        if (canvasType == CANVAS_PARTICLES) {
+            drawParticles()
+        } else if (canvasType == CANVAS_SNAKE) {
+            drawSnake()
+        } else {
+            drawAsteroids()
+        }
+    }
 
-			let probabilityToTurn = Math.random() > 0.7
+    function drawParticles() {
+        ctxMain.clearRect(0, 0, width, height)
+        for (let i = 0; i < dots.length; i++) {
+            for (let j = 0; j < dots.length; j++) {
+                if (i != j) {
+                    dots[j].check(dots[i], dist)
+                }
+            }
+            dots[i].show()
+            dots[i].update()
+        }
+        requestId = requestAnimationFrame(drawParticles)
+    }
 
-			if (probabilityToTurn) {
-				snake.move(getRandomIntFromInterval(1, 4))
-			}
+    function drawSnake() {
+        let currentTime = Date.now()
 
-			updatedTime = currentTime
-		}
+        if ((currentTime - updatedTime) >= 200) {
+            ctxMain.clearRect(0, 0, width, height)
 
-		requestId = requestAnimationFrame(drawSnake)
-	}
+            snake.update()
+            snake.show()
 
-	function drawAsteroids() {
-		ctxMain.clearRect(0, 0, width, height)
-		spaceShip.show()
+            let probabilityToTurn = Math.random() > 0.7
 
-		let probabilityToTurn = Math.random() > 0.7
+            if (probabilityToTurn) {
+                snake.move(getRandomIntFromInterval(1, 4))
+            }
 
-		if (probabilityToTurn) {
-			asteroidKeyControl = getRandomIntFromInterval(1, 4)
+            updatedTime = currentTime
+        }
 
-			switch (asteroidKeyControl) {
-				case 1:
-					spaceShip.turnLeft()
-					break
-	
-				case 2:
-					spaceShip.turnRight()
-					break
-	
-				case 3:
-					spaceShip.thrust()
-					break
-	
-				case 4:
-					spaceShip.fire()
-					break
-			}
-		}
+        requestId = requestAnimationFrame(drawSnake)
+    }
 
-		spaceShip.update()
+    function drawAsteroids() {
+        ctxMain.clearRect(0, 0, width, height)
+        spaceShip.show()
 
-		for (let i = 0; i < asteroids.length; i++) {
-			asteroids[i].show()
-			asteroids[i].update()
-			for (let j = 0; j < spaceShip.laser.length; j++) {
-				let d = distance(asteroids[i].posX, asteroids[i].posY, spaceShip.laser[j].posX, spaceShip.laser[j].posY)
-				if (d < asteroids[i].radius) {
-					asteroids.splice(i, 1)
-					spaceShip.laser.splice(j, 1)
-					asteroids.push(new Asteroid(ctxMain, 28, width, height))
-				}
-			}
-		}
+        let probabilityToTurn = Math.random() > 0.7
+
+        if (probabilityToTurn) {
+            asteroidKeyControl = getRandomIntFromInterval(1, 4)
+
+            switch (asteroidKeyControl) {
+                case 1:
+                    spaceShip.turnLeft()
+                    break
+
+                case 2:
+                    spaceShip.turnRight()
+                    break
+
+                case 3:
+                    spaceShip.thrust()
+                    break
+
+                case 4:
+                    spaceShip.fire()
+                    break
+            }
+        }
+
+        spaceShip.update()
+
+        for (let i = 0; i < asteroids.length; i++) {
+            asteroids[i].show()
+            asteroids[i].update()
+            for (let j = 0; j < spaceShip.laser.length; j++) {
+                let d = distance(asteroids[i].posX, asteroids[i].posY, spaceShip.laser[j].posX, spaceShip.laser[j].posY)
+                if (d < asteroids[i].radius) {
+                    asteroids.splice(i, 1)
+                    spaceShip.laser.splice(j, 1)
+                    asteroids.push(new Asteroid(ctxMain, 28, width, height))
+                }
+            }
+        }
 
 
         requestId = requestAnimationFrame(drawAsteroids)
-	}
+    }
 
-	window.addEventListener('resize', (ev => {
-		dots.length = 0
-		asteroids.length = 0
-		cancelAnimationFrame(requestId)
-		setupCanvas()
-		draw()
-	}))
+    window.addEventListener('resize', (ev => {
+        dots.length = 0
+        asteroids.length = 0
+        cancelAnimationFrame(requestId)
+        setupCanvas()
+        draw()
+    }))
 
-	if (window.innerWidth < 767) {
-		n = 16
-		dist = 160
-		asteroidCount = 8
-	}
+    if (window.innerWidth < 767) {
+        n = 16
+        dist = 160
+        asteroidCount = 8
+    }
 
-	setupEnvironment()
-	setupCanvas()
-	draw()
+    setupEnvironment()
+    setupCanvas()
+    draw()
 }
